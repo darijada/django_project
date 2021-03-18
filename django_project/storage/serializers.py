6992893d13ead533940a85b1253a58d7bce3c152
darijada <darija.dabic@yahoo.com>
2021-03-18 20:31:58 +0100

    [DEV] storage - initialization

---
+++ django_project/storage/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from base.serializers import CitySerializer
+from user.serializers import UserProfileSerializer
+from .models import Storage, Classification
+from parler_rest.serializers import TranslatableModelSerializer
+from parler_rest.fields import TranslatedFieldsField
+from config.mixins import TranslatedSerializerMixin
+
+
+class ClassificationSerializer(TranslatedSerializerMixin, TranslatableModelSerializer):
+    translations = TranslatedFieldsField(shared_model=Classification)
+
+    class Meta:
+        model = Classification
+        fields = '__all__'
+
+
+class StorageSerializer(serializers.ModelSerializer):
+    city = CitySerializer(many=False)
+    classifications = ClassificationSerializer(many=True)
+    user = UserProfileSerializer(read_only=True)
+    
+    class Meta:
+        model = Storage
+        fields = '__all__'
---
+++ django_project/storage/urls.py
@@ -0,0 +1,7 @@
+from django.urls import include, path
+from . import views
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'', views.StorageViewSet)
